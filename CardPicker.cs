01a2116c7c32f37be4e05e64f278d0697178fc85
Archi-Paweu <p.s.wisniewski@gmail.com>
2022-09-01 18:42:02 +0200

    Wybieranie losowej karty (z możliwym powtórzeniem tej samej karty).

---
+++ CardPicker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACard
+{
+     class CardPicker
+     {
+        static Random random = new Random();
+        public static string[] PickACard(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++) 
+            {
+                pickedCards[i] = randomValue() + " " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1) return "karo";
+            if (value == 2) return "kier";
+            if (value == 3) return "pik";
+            else return "trefl";
+        }
+
+        private static string randomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "as";
+            if (value == 11) return "valet";
+            if (value == 12) return "dama";
+            if (value == 13) return "król";
+            else return value.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PickACard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpisz liczbę generowanych kart: ");
+
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickACard(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Wpisz prawidłową liczę.");
+            }
+        }
+    }
+}
